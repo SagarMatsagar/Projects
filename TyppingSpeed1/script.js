ba017093702ddb636d3695631006eebe55bef6dd
SagarMatsagar <95484469+SagarMatsagar@users.noreply.github.com>
2022-02-11 11:38:41 +0530

    Update script.js

--- TyppingSpeed1/script.js
+++ TyppingSpeed1/script.js
@@ -3,7 +3,7 @@ const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
 
-quoteInputElement.addEventListener('input', () => {
+quoteInputElement.addEventListener(input, () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
   const arrayValue = quoteInputElement.value.split('')
 
